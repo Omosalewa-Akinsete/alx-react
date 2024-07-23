2aea47b222075e1d80b38eae4c66754b20fe2a72
Omosalewa-Akinsete <shaleycoral@gmail.com>
2024-07-23 16:01:20 -0700

    Learning how to use Webpack with a config file

--- 0x00-Webpack/task_1/js/dashboard_main.js
+++ 0x00-Webpack/task_1/js/dashboard_main.js
@@ -1,24 +1,22 @@
-// Import jQuery and Lodash
+// Import jQuery and Lodash libraries
 import $ from 'jquery';
 import _ from 'lodash';
 
+// Add elements to the body of the document
+$('body').append('<p>Holberton Dashboard</p>'); // Adds a paragraph with text "Holberton Dashboard"
+$('body').append('<p>Dashboard data for the students</p>'); // Adds a paragraph with text "Dashboard data for the students"
+$('body').append('<button>Click here to get started</button>'); // Adds a button with text "Click here to get started"
+$('body').append('<p id="count"></p>'); // Adds an empty paragraph with id "count" for displaying click count
+$('body').append('<p>Copyright - Holberton School</p>'); // Adds a paragraph with text "Copyright - Holberton School"
+
+// Initialize a counter variable
 let count = 0;
 
-// Function to update the counter
+// Define a function to update the counter and display the click count
 function updateCounter() {
-  count += 1;
-  $('#count').text(`${count} clicks on the button`);
-}
+    count++; // Increment the counter
+    $('#count').html(`${count} clicks on the button`); // Update the content of the paragraph with id "count"
+};
 
-// Document ready function
-$(document).ready(function() {
-  // Create and append the elements
-  $('body').append('<p>Holberton Dashboard</p>');
-  $('body').append('<p>Dashboard data for the students</p>');
-  $('body').append('<button id="click-button">Click here to get started</button>');
-  $('body').append('<p id="count"></p>');
-  $('body').append('<p>Copyright - Holberton School</p>');
-
-  // Bind the debounced updateCounter function to the button's click event
-  $('#click-button').on('click', _.debounce(updateCounter, 300));
-});
+// Bind the updateCounter function to the button's click event with a debounce of 500ms
+$('button').on('click', _.debounce(updateCounter, 500));
