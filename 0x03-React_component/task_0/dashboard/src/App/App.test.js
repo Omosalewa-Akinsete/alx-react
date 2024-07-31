f5a1e30e5db688a194b75f21eb12eb4fbe70f795
Omosalewa Akinsete <141264646+Omosalewa-Akinsete@users.noreply.github.com>
2024-07-31 08:18:54 -0700

    Commence with class components

--- 0x03-React_component/task_0/dashboard/src/App/App.test.js
+++ 0x03-React_component/task_0/dashboard/src/App/App.test.js
@@ -1,11 +1,16 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import App from './App';
+import Notifications from '../Notifications/Notifications';
+import Header from '../Header/Header';
 import Login from '../Login/Login';
 import CourseList from '../CourseList/CourseList';
 import Footer from '../Footer/Footer';
 
-describe('<App />', () => {
+describe('<App/>', () => {
   describe('Does not crash', () => {
     it('renders without crashing', () => {
-      shallow(<App />);
+      shallow(<App/>);
     });
   });
 
@@ -13,7 +18,7 @@ describe('<App />', () => {
     let wrapper;
 
     beforeEach(() => {
-      wrapper = shallow(<App />);
+      wrapper = shallow(<App/>);
     });
 
     it('should contain the Notifications component', () => {
@@ -21,33 +26,33 @@ describe('<App />', () => {
     });
 
     it('should contain the Header component', () => {
-      wrapper = shallow(<App />);
+      wrapper = shallow(<App/>);
       expect(wrapper.find(Header)).toHaveLength(1);
     });
 
     it('should contain the Login component', () => {
-      wrapper = shallow(<App />);
+      wrapper = shallow(<App/>);
       expect(wrapper.find(Login)).toHaveLength(1);
     });
 
     it('should contain the Footer component', () => {
-      wrapper = shallow(<App />);
+      wrapper = shallow(<App/>);
       expect(wrapper.find(Footer)).toHaveLength(1);
     });
 
     it('does not display CourseList', () => {
-      wrapper = shallow(<App />);
+      wrapper = shallow(<App/>);
       expect(wrapper.find(CourseList)).toHaveLength(0);
     });
   });
 });
 
-describe('<App isLoggedIn=true />', () => {
+describe('<App isLoggedIn=true/>', () => {
   describe('Displays correct component', () => {
     let wrapper;
 
     beforeEach(() => {
-      wrapper = shallow(<App isLoggedIn={true} />);
+      wrapper = shallow(<App isLoggedIn={true}/>);
     });
 
     it('does not display the Login component', () => {
@@ -55,7 +60,7 @@ describe('<App isLoggedIn=true />', () => {
     });
 
     it('renders the CourseList component', () => {
-      wrapper = shallow(<App isLoggedIn={true} />);
+      wrapper = shallow(<App isLoggedIn={true}/>);
       expect(wrapper.find(CourseList)).toHaveLength(1);
     });
   });
