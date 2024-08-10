77f3978c71096bce61c9724d9f96452021bf2ccf
Omosalewa-Akinsete <shaleycoral@gmail.com>
2024-08-09 21:44:55 -0700

    Modify the Notifications

--- 0x01-react_intro/task_2/dashboard/src/App.js
+++ 0x01-react_intro/task_2/dashboard/src/App.js
@@ -1,19 +1,30 @@
 import React from 'react';
-import logo from './holberton-logo.png'; // Make sure the logo image is in the correct path
+import logo from './holberton-logo.jpg'; // Make sure the logo image is in the correct path
 import './App.css';
+import {getFullYear, getFooterCopy} from './utils';
+import { Notifications } from './Notifications';
 
 function App() {
   return (
     <div className="App">
+      <Notifications/>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={logo} className="App-logo" alt="logo"/>
         <h1>School dashboard</h1>
       </header>
       <div className="App-body">
         <p>Login to access the full dashboard</p>
+	<p>Login to access the full dashboard</p>
+        <label htmlFor="email">Email:</label>
+        <input type="email" id="email" name="email"/>
+        <br/>
+        <label htmlFor="password">Password:</label>
+        <input type="password" id="password" name="password"/>
+        <br/>
+        <button>OK</button>
       </div>
       <footer className="App-footer">
-        <p>Copyright 2020 - Holberton School</p>
+        <p>{getFooterCopy(true)} - {getFullYear()}</p>
       </footer>
     </div>
   );
