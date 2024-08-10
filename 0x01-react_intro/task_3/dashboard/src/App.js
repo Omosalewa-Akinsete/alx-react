fa1de128b0483d8a1a0ef6f239022f45e2afffa9
Omosalewa-Akinsete <shaleycoral@gmail.com>
2024-08-09 21:48:28 -0700

    Create React tests

--- 0x01-react_intro/task_3/dashboard/src/App.js
+++ 0x01-react_intro/task_3/dashboard/src/App.js
@@ -1,20 +1,23 @@
-import React from 'react';
-import logo from './holberton-logo.png'; // Make sure the logo image is in the correct path
+import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>School dashboard</h1>
+        <img src={logo} className="App-logo" alt="logo"/>
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
       </header>
-      <div className="App-body">
-        <p>Login to access the full dashboard</p>
-      </div>
-      <footer className="App-footer">
-        <p>Copyright 2020 - Holberton School</p>
-      </footer>
     </div>
   );
 }
