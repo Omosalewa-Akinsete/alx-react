4da2cc3318759fc30f0d42bb1ae17ded40c57e47
Omosalewa-Akinsete <shaleycoral@gmail.com>
2024-08-09 21:18:24 -0700

    Basic application

--- 0x01-react_intro/task_0/dashboard/src/App.js
+++ 0x01-react_intro/task_0/dashboard/src/App.js
@@ -1,17 +1,16 @@
-import React from 'react';
-import logo from './holberton-logo.png'; // Make sure the logo image is in the correct path
+import logo from './holberton-logo.jpg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>School dashboard</h1>
+        <img src={logo} className="App-logo" alt="Holberton-logo"/>
+	<h1>School dashboard</h1>
       </header>
-      <div className="App-body">
+      <body className="App-body">
         <p>Login to access the full dashboard</p>
-      </div>
+      </body>
       <footer className="App-footer">
         <p>Copyright 2020 - Holberton School</p>
       </footer>
