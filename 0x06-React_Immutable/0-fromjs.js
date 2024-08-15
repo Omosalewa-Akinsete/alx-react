22c28dca429245fe8536e344bb056679a9eab471
Omosalewa-Akinsete <shaleycoral@gmail.com>
2024-08-14 22:44:55 -0700

    Converting into an Immutable object using fromJS

--- 0x06-React_Immutable/0-fromjs.js
+++ 0x06-React_Immutable/0-fromjs.js
@@ -1,12 +1,5 @@
-const { fromJS } = require('immutable');
+import { fromJS } from 'immutable';
 
-/**
- * Converts a JavaScript object into an Immutable.js Map.
- * @param {Object} object - The object to convert.
- * @returns {Map} - The immutable Map created from the object.
- */
-function getImmutableObject(object) {
-    return fromJS(object);
+export default function getImmutableObject(object) {
+  return fromJS(object);
 }
-
-module.exports = getImmutableObject;
