d8e2c64008cce0fb40477f663d4ebd962915aa9e
Omosalewa-Akinsete <shaleycoral@gmail.com>
2024-08-30 22:04:13 -0700

    Commence with class components

--- 0x03-React_component/task_0/dashboard/src/App.js
+++ 0x03-React_component/task_0/dashboard/src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import './App.css';
+import Notifications from './Notifications/Notifications';
+import Header from './Header/Header';
+import Footer from './Footer/Footer';
+import Login from './Login/Login';
+import CourseList from './CourseList/CourseList'; // Import CourseList component
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  // Define listNotifications and listCourses as class properties
+  listNotifications = [
+    { id: 1, type: 'default', value: 'New course available' },
+    { id: 2, type: 'urgent', value: 'New resume available' },
+    { id: 3, type: 'default', html: { __html: '<strong>Meeting at 3pm</strong>' } }
+  ];
+
+  // Create a new array named listCourses
+  listCourses = [
+    { id: 1, name: 'ES6', credit: 60 },
+    { id: 2, name: 'Webpack', credit: 20 },
+    { id: 3, name: 'React', credit: 40 }
+  ];
+
+  render() {
+    const { isLoggedIn = false } = this.props;
+
+    return (
+      <>
+        <Notifications displayDrawer listNotifications={this.listNotifications}/>
+        <div className="App">
+          <Header/>
+          <div className="App-body">
+            {isLoggedIn ? <CourseList listCourses={this.listCourses}/> : <Login/>}
+          </div>
+          <Footer/>
+        </div>
+      </>
+    );
+  }
 }
+
+App.propTypes = {
+  isLoggedIn: PropTypes.bool,
+};
 
 export default App;
